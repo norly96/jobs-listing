16138044135afe6a1e012e405e096f64f82bd03f
Norluis Galvez <norly021196@gmail.com>
2024-09-18 01:13:41 -0400

    fix style responsive

--- src/components/FilterItem.tsx
+++ src/components/FilterItem.tsx
@@ -1,8 +1,8 @@
 const FilterItem = ({ name }: { name: string }) => {
   return (
     <div className="bg-gray-200 rounded-md flex items-center">
-      <h1 className="px-2">{name}</h1>
-      <button className="bg-dark-cyan hover:bg-black py-3 px-2 rounded-r-md">
+      <h1 className="px-2 text-sm sm:text-md lg:text-lg">{name}</h1>
+      <button className="bg-dark-cyan hover:transition-all hover:bg-black py-3 px-2 rounded-r-md">
         <img src="/images/icon-remove.svg" alt="" />
       </button>
     </div>
